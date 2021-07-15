b4f4d441e1e17ae1a523d1e8cd0ea3170d16308e
Nicole Siqueira Fagundes <psi.fagundes@gmail.com>
2021-07-15 12:00:22 -0300

    Primeiro Commit

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/Services/JogoCadastradoException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ApiCatalogoJogos.Services
+{
+    [Serializable]
+    internal class JogoCadastradoException : Exception
+    {
+        public JogoCadastradoException()
+        {
+        }
+
+        public JogoCadastradoException(string message) : base(message)
+        {
+        }
+
+        public JogoCadastradoException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected JogoCadastradoException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
